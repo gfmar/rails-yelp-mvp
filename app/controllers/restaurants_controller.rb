8f42d61b839b66aaffc6db432c7ecbbb2c14eabb
Gustaf Folkmar <gustaf@folkmar.se>
2019-05-15 18:17:48 -0400

    Basic functionality in place. Missing css

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
-  before_action :find_restaurant, only: [:show, :edit, :update]
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+    # will render page
   end
 
   def new
@@ -15,25 +16,39 @@ class RestaurantsController < ApplicationController
   def create
     # Recevie from params
     # Create and save
-    # Redirect
+    @restaurant = Restaurant.new(params_restaurant)
+
+    if @restaurant.save
+      # Redirect
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
   end
 
   def edit
-    #
+    # Will generate form
   end
 
   def update
-    #
+    if @restaurant.update(params_restaurant)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
-    #
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
-
   def find_restaurant
     @restaurant = Restaurant.find(params['id'])
   end
 
+  def params_restaurant
+    params.require('restaurant').permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
+
   def create
+    @restaurant = Restaurant.find(params['restaurant_id'])
+
+    @review = Review.new(params_review)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def new
+    @restaurant = Restaurant.find(params['restaurant_id'])
+    @review = Review.new
+  end
+
+  private
+
+  def params_review
+    params.require('review').permit(:content, :rating)
   end
 end
